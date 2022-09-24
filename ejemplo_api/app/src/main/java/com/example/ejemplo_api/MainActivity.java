b28540ddcf8a19bffc871789c3a432d77c08cb73
ignaciobarbieri <ignaciobarbieri2003@gmail.com>
2022-09-23 21:43:27 -0300

    aaaaaaaaaaaaaaaaaaaaaaaaaaa

--- ejemplo_api/app/src/main/java/com/example/ejemplo_api/MainActivity.java
+++ ejemplo_api/app/src/main/java/com/example/ejemplo_api/MainActivity.java
@@ -3,11 +3,27 @@ package com.example.ejemplo_api;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 public class MainActivity extends AppCompatActivity {
+    private  RequestQueue mRequestQueue;
+    private  StringRequest mStringRequest;
+
+    private EditText edad;
+
+    private String url = "http:/192.168.137.1/androidusers/buscar.php";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,11 +47,27 @@ public class MainActivity extends AppCompatActivity {
         SH.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
+                    sendAndRequestResponse();
                     startActivity(juan);
                 }
 
 
     });
 
+    }
+    private void sendAndRequestResponse() {
+        mRequestQueue= Volley.newRequestQueue(this);
+        mStringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Log.e("HttpClient","success! response: "+response.toString());
+                Toast.makeText(getApplicationContext(),"Response: "+ response.toString());
+
+            }
+        });
+
+    }
+    private void onErrorResponse(volleyError error){
+        Log.e(TAG,"error: " +error.toString);
     }
 }
